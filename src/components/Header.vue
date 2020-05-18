089fdddd10113a8d2706cfc1f6a098c2d488c158
dsgraphics <davidsanders5295@gmail.com>
2020-05-18 18:01:21 +0100

    Initial

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <header class="header">
+    <h1>Dave is really Skill</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    msg: String
+  }
+}
+</script>
